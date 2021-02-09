e302dc05b289d9bcc0bd087c4aa2ebdcde5c8ecc
Yi_Ana <46570960+AnastasiaYiChen@users.noreply.github.com>
2021-02-09 15:43:20 -0400

    Add files via upload

---
+++ C#M01_SalarySystem/main_cs/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Manager : Employee //derived from Employee (child of Employee)
+    {
+        public Manager(string firstName, string lastname, string address, decimal salary, decimal performanceRating)
+        {
+            Firstname = firstName;
+            Lastname = lastname;
+            Address = address;
+            Salary = salary;
+            PerformanceRating = performanceRating;
+
+        }
+    }
+
+}
